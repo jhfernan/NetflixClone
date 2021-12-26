cc7104454d3065f73b94a5f08b3f86d86648500a
Jon Fei <jhfernan@mail.usf.edu>
2021-12-26 13:37:13 +0800

    Initial Commit

---
+++ netflixBuild/netflixBuildApp.swift
@@ -0,0 +1,17 @@
+//
+//  netflixBuildApp.swift
+//  netflixBuild
+//
+//  Created by Jonathan Fernandes on 12/26/21.
+//
+
+import SwiftUI
+
+@main
+struct netflixBuildApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
